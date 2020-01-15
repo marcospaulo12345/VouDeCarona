a31300af78fb593bd502b93e47b9b7301e127157
Marcos Paulo <marcospaulo.mpf34@gmail.com>
2020-01-14 22:44:28 -0300

    Iniciando a criação do layout da pagina index

--- VouDeCarona/urls.py
+++ VouDeCarona/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from carona import views
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('index/', views.index),
+    path('', views.index),
 ]
