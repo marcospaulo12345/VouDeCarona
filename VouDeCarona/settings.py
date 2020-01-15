23ae2f4baf5039a0294cafa8e0009bb36c654a12
Marcos Paulo <marcospaulo.mpf34@gmail.com>
2020-01-14 21:42:28 -0300

    Criação das pastas templates e static para criação do frontend do projeto

--- VouDeCarona/settings.py
+++ VouDeCarona/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'VouDeCarona.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -99,7 +99,10 @@ AUTH_PASSWORD_VALIDATORS = [
     },
 ]
 
-
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    '/var/www/static/',
+]
 # Internationalization
 # https://docs.djangoproject.com/en/2.2/topics/i18n/
 
